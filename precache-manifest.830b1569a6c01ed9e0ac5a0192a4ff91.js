b5d5677eafc5d80dbcb84dad04dc253d548850c8
yanjianli <258689082@qq.com>
2020-01-14 18:10:50 +0800

    7

---
+++ precache-manifest.830b1569a6c01ed9e0ac5a0192a4ff91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5833a0a4efc2e5499db740ddd9a4f308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34c557101aab7c23f2a6",
+    "url": "/static/js/2.56b7db38.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.56b7db38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5c21dde859237ae397e",
+    "url": "/static/js/main.be9dcab4.chunk.js"
+  },
+  {
+    "revision": "961b06232e59ea10332f",
+    "url": "/static/js/runtime-main.c8114503.js"
+  }
+]);
